a0d91dfa896846e387b382d86d99e2a2570237f5
Satej Kulkarni <149229375+satej3@users.noreply.github.com>
2026-01-13 18:08:36 +0530

    Update api.js - async keyword was missing

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-export const analyzeResume = (formData) =>
+export const analyzeResume = async (formData) =>
   await fetch(`${API_BASE_URL}/api/analysis/analyze`, {
   method: "POST",
   body: formData,
 });
+
 
