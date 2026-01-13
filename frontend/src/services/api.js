7d8d311ec213c049100cf69c3218c5afbe88f43b
Satej Kulkarni <149229375+satej3@users.noreply.github.com>
2026-01-13 17:45:32 +0530

    Updated api.js for dynamic backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,9 @@
 import axios from "axios";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const analyzeResume = (formData) =>
-  axios.post("http://localhost:5000/api/analysis/analyze", formData);
+  await fetch(`${API_BASE_URL}/api/analysis/analyze`, {
+  method: "POST",
+  body: formData,
+});
+
