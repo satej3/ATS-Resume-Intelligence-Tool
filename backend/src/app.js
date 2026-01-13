c5f44ab085fd5e8c56e3a0b0860cb6610d674a81
Satej Kulkarni <149229375+satej3@users.noreply.github.com>
2026-01-13 17:04:26 +0530

    Updated app.js for cors setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,12 +4,25 @@ const analysisRoutes = require("./routes/analysis.routes");
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.FRONTEND_URL
+];
+
 app.use(
-    cors({
-    origin: "http://localhost:5173",
+     cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
-  })//allowed from our frontend only for cors
+  })
 );
 
 app.use(express.json());
@@ -17,3 +30,4 @@ app.use(express.json());
 app.use("/api/analysis", analysisRoutes);
 
 module.exports = app;
+
