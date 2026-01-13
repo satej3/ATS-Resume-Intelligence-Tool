2e7ee4aaabad7bd4848ba1b9dd6ce8f17b6688c8
Satej Kulkarni <149229375+satej3@users.noreply.github.com>
2026-01-13 18:47:52 +0530

    Update api.js - response changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,17 @@
 import axios from "axios";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-export const analyzeResume = async (formData) =>
-  await fetch(`${API_BASE_URL}/api/analysis/analyze`, {
-  method: "POST",
-  body: formData,
-});
-
-
+export const analyzeResume = async (formData) => {
+  const response = await fetch(`${API_BASE_URL}/api/analysis/analyze`, {
+    method: "POST",
+    body: formData,
+  });
+  
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  
+  // Parse JSON from response
+  const data = await response.json();
+  return { data }; 
+};
